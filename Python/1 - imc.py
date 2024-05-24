7ffd2c839e563660cc7c34b12ac1e9318029143f
Ariel Acosta <j.ariel.acosta17@gmail.com>
2024-05-23 21:14:25 -0500

    Assignment 1 with try-catch Python

--- Python/1 - imc.py
+++ Python/1 - imc.py
@@ -1,22 +1,31 @@
-peso = float(input("Introduzca su peso en kilogramos"))
-altura = float(input("Introduzca su altura en metros"))
+try:
+    peso = (input("Introduzca su peso en kilogramos: "))
+    peso = float(peso)
 
-imc = peso / altura**2
+    altura = float(input("Introduzca su altura en metros: "))
+    altura = float(altura)
 
-print("Su indice de masa corporal es:", f"{imc:.2f}")
-indicador = ""
+    imc = peso / altura**2
+    
+    print("Su indice de masa corporal es: ", f"{imc:.2f}")
+    indicador = ""
 
-if imc < 18.5:
-    indicador = "BAJO PESO"
-elif imc <= 24.9:
-    indicador = "PESO NORMAL"
-elif imc <= 29.9:
-    indicador = "SOBREPESO"
-elif imc <= 34.9:
-    indicador = "OBESIDAD TIPO 1"
-elif imc <= 39.9:
-    indicador = "OBESIDAD TIPO 2"
-elif imc >= 40:
-    indicador = "OBESIDAD TIPO 3"
+    if imc < 18.5:
+        indicador = "BAJO PESO"
+    elif imc <= 24.9:
+        indicador = "PESO NORMAL"
+    elif imc <= 29.9:
+        indicador = "SOBREPESO"
+    elif imc <= 34.9:
+        indicador = "OBESIDAD TIPO 1"
+    elif imc <= 39.9:
+        indicador = "OBESIDAD TIPO 2"
+    elif imc >= 40:
+        indicador = "OBESIDAD TIPO 3"
 
-print("Tu indice de masa corporal indica: ", indicador)
+    print("Este indice indica: ", indicador)
+
+except ValueError:
+    print("Error: El peso y la altura deben ser valores numericos")
+except ZeroDivisionError:
+    print("Error: La division entre cero no es permitida")
