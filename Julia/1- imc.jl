5b4eb4223c5e389747750b9c68bb50f7afd89b5a
Ariel Acosta <j.ariel.acosta17@gmail.com>
2024-05-23 21:58:59 -0500

    Add try-catch to assignment 1 Julia

--- Julia/1- imc.jl
+++ Julia/1- imc.jl
@@ -1,26 +1,38 @@
-println("Introduzca su peso en kilogramos")
-peso = parse(Float16, readline())
+try
+        
+    println("Introduzca su peso en kilogramos: ")
+    peso = parse(Float16, readline())
 
-println("Introduzca su altura en metros")
-altura = parse(Float16, readline())
+    println("Introduzca su altura en metros: ")
+    altura = parse(Float16, readline())
 
-imc = peso / altura ^ 2
+    imc = peso / altura ^ 2
 
-println("Su indice de masa corporal es: $imc")
-indicador = ""
+    println("Su indice de masa corporal es: $imc")
+    indicador = ""
 
-if imc < 18.5
-    indicador = "BAJO PESO"
-elseif imc <= 24.91
-    indicador = "PESO NORMAL"
-elseif imc <= 29.9
-    indicador = "SOBREPESO"
-elseif imc <= 34.9
-    indicador = "OBESIDAD TIPO 1"
-elseif imc <= 39.9
-    indicador = "OBESIDAD TIPO 2"
-elseif imc >= 40
-    indicador = "OBESIDAD TIPO 3"
+    if imc < 18.5
+        indicador = "BAJO PESO"
+    elseif imc <= 24.91
+        indicador = "PESO NORMAL"
+    elseif imc <= 29.9
+        indicador = "SOBREPESO"
+    elseif imc <= 34.9
+        indicador = "OBESIDAD TIPO 1"
+    elseif imc <= 39.9
+        indicador = "OBESIDAD TIPO 2"
+    elseif imc >= 40
+        indicador = "OBESIDAD TIPO 3"
+    end
+
+    print("Este indice indica: ", indicador)
+
+catch e
+    if isa(e, ArgumentError)
+        println("Error: El peso y la altura deben ser valores numericos")
+    elseif isa(e, DivideError)
+        print("Error: La division entre cero no esta permitida")
+    else
+        rethrow(e)
+    end
 end
-
-print("Su indice de masa corporal indica: ", indicador)
