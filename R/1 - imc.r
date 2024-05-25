8f04faec7e85d2c4bfeeb0294c881837e3b7c546
Ariel Acosta <j.ariel.acosta17@gmail.com>
2024-05-24 19:58:01 -0500

    Add try catch to R, missing validation still

--- R/1 - imc.r
+++ R/1 - imc.r
@@ -1,18 +1,38 @@
-peso <- readline(prompt = "Introduzca su peso en kilogramos: ")
-altura <- readline(prompt = "Introduzca su altura en metros: ")
+tryCatch({
 
-imc <- as.numeric(peso) / as.numeric(altura) ^ 2
+  peso <- as.numeric(readline(prompt = "Introduzca su peso en kilogramos: "))
+  altura <- readline(prompt = "Introduzca su altura en metros: ")
 
-imc_2 <- sprintf("%.2f", imc)
+  if (!is.na(peso) || !is.na(altura)){
+    stop("Error de valores: El peso y la altura deben ser valores numericos")
+  }
+  if (as.numeric(altura) == 0) {
+    stop("Error matematico: La division entre cero no es permitida")
+  }
+  imc <- as.numeric(peso) / as.numeric(altura) ^ 2
 
-print(paste("Su indice de masa corporal es:", imc_2))
-indicador <- ""
+  imc_2 <- sprintf("%.2f", imc)
 
-if (imc < 18.5) indicador <- "BAJO PESO" else
-  if (imc <= 24.9) indicador <- "PESO NORMAL" else
-  if (imc <= 29.9) indicador <- "SOBREPESO" else
-  if (imc <= 34.9) indicador <- "OBESIDAD TIPO 1" else
-  if (imc <= 39.9) indicador <- "OBESIDAD TIPO 2" else
-  if (imc >= 40) indicador <- "OBESIDAD TIPO 3"
+  cat("Su indice de masa corporal es: ", imc_2, "\n")
+  indicador <- ""
 
-print(paste("Su indice de masa corporal indica: ", indicador))
+  if (imc < 18.5) indicador <- "BAJO PESO" else
+    if (imc <= 24.9) indicador <- "PESO NORMAL" else
+    if (imc <= 29.9) indicador <- "SOBREPESO" else
+    if (imc <= 34.9) indicador <- "OBESIDAD TIPO 1" else
+    if (imc <= 39.9) indicador <- "OBESIDAD TIPO 2" else
+    if (imc >= 40) indicador <- "OBESIDAD TIPO 3"
+
+  cat("Este indice indica: ", indicador)
+  },
+  error = function(e) {
+    # grepl verifica si el mensaje de error contiene la cadena "Error de valores"
+    if (grepl("Error de valores", e$message)) {
+      cat("Error: El peso y la altura deben ser valores numericos.\n")
+    } else if (grepl("Error matematico", e$message)) {
+      cat("Error: La division entre cero no esta permitida. \n") 
+      } else {
+        cat("Un error inesperado ha ocurrido: ", e$message, "\n")
+      }
+    }
+)
