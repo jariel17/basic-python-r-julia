570ee6db220afde1b6ae55dce9bf2e54f9a4ecc2
Ariel Acosta <j.ariel.acosta17@gmail.com>
2024-06-27 21:01:03 -0500

    Better if handling task 1

--- R/1 - imc.r
+++ R/1 - imc.r
@@ -9,20 +9,27 @@ tryCatch({
   if (as.numeric(altura) == 0) {
     stop("Error matematico: La division entre cero no es permitida")
   }
-  imc <- peso / altura ^ 2
+  imc <- peso / altura^2
 
   imc_2 <- sprintf("%.2f", imc)
 
   cat("Su indice de masa corporal es: ", imc_2, "\n")
 
-  if (imc < 18.5) indicador <- "BAJO PESO" else
-    if (imc <= 24.9) indicador <- "PESO NORMAL" else
-    if (imc <= 29.9) indicador <- "SOBREPESO" else
-    if (imc <= 34.9) indicador <- "OBESIDAD TIPO 1" else
-    if (imc <= 39.9) indicador <- "OBESIDAD TIPO 2" else
-    if (imc >= 40) indicador <- "OBESIDAD TIPO 3"
+  indicador <- if (imc < 18.5) {
+    "BAJO PESO"
+  } else if (imc <= 24.9) {
+    "PESO NORMAL"
+  } else if (imc <= 29.9) {
+    "SOBREPESO"
+  } else if (imc <= 34.9) {
+    "OBESIDAD TIPO 1"
+  } else if (imc <= 39.9) {
+    "OBESIDAD TIPO 2"
+  } else {
+    "OBESIDAD TIPO 3"
+  }
 
-  cat("Este indice indica: ", indicador)
+  cat("Este indice indica: ", indicador, "\n")
   },
   error = function(e) {
     # grepl verifica si el mensaje de error contiene la cadena "Error de valores"
